58f346868c6812ea9d43bb990ea6205a0d90d1f5
Nicolas Anzevino <NicolasAnzevino@hotmail.com>
2024-10-09 22:13:27 -0300

    fix

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -1,12 +1,11 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId,
+  apiKey: "AIzaSyAqVfFjAUo3L3O4GqNeNTZt1bDZrHos8rw",
+  authDomain: "zonapixel-coder.firebaseapp.com",
+  projectId: "zonapixel-coder",
+  storageBucket: "zonapixel-coder.appspot.com",
+  messagingSenderId: "1067422478990",
+  appId: "1:1067422478990:web:a5fe1d295339d599840fdf"
 };
-
 export const app = initializeApp(firebaseConfig);
