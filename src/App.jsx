72e3497ec302dd8d630c435fb63324a58bd2d61e
Nicolas Anzevino <NicolasAnzevino@hotmail.com>
2024-10-15 16:58:48 -0300

    fix final

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import AppRouter from './router/AppRouter'
-import CartProvider from './context/cartProvider'
+import CartProvider from './context/CartProvider'
 
 function App() {
   return (<CartProvider><AppRouter/></CartProvider>)
